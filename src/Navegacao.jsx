80f810501703c58c7dac49b43a5faa58b36ea563
Diego Paiva <www.diegopaiva@gmail.com>
2023-06-28 14:46:49 -0300

    Icons navbar

--- src/Navegacao.jsx
+++ src/Navegacao.jsx
@@ -2,21 +2,22 @@ import React, { useState, useEffect } from 'react';
 import { NavLink, useNavigate } from 'react-router-dom';
 import { Navbar, Container, Nav, Button } from 'react-bootstrap';
 import { onAuthStateChanged, signOut } from 'firebase/auth';
+import { FiHome, FiUser, FiLogOut, FiLogIn, FiImage } from 'react-icons/fi';
 import logo from './images/logo.png';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.bundle.js';
 import './Navegacao.css';
-import {auth} from './firebaseConfig'
-
-
+import { auth, firestore } from './firebaseConfig';
 
 function Navegacao() {
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [currentUser, setCurrentUser] = useState(null);
   const navigate = useNavigate();
 
   useEffect(() => {
     const unsubscribe = onAuthStateChanged(auth, (user) => {
       setIsLoggedIn(!!user);
+      setCurrentUser(user);
     });
 
     return () => unsubscribe();
@@ -26,10 +27,31 @@ function Navegacao() {
     try {
       await signOut(auth);
       setIsLoggedIn(false);
+      setCurrentUser(null);
       navigate('/login');
     } catch (error) {
       console.error('Erro ao fazer logout:', error);
     }
+  };
+
+  const getProfileUrl = async () => {
+    if (currentUser) {
+      try {
+        const userDocRef = firestore.collection('users').doc(currentUser.uid);
+        const userDocSnapshot = await userDocRef.get();
+
+        if (userDocSnapshot.exists()) {
+          const userData = userDocSnapshot.data();
+          if (userData && userData.profileUrl) {
+            return userData.profileUrl;
+          }
+        }
+      } catch (error) {
+        console.error('Erro ao obter informações do usuário:', error);
+      }
+    }
+
+    return '/perfil';
   };
 
   return (
@@ -43,28 +65,35 @@ function Navegacao() {
             className="d-inline-block align-top logo"
           />
         </Navbar.Brand>
-        <Navbar.Toggle aria-controls="basic-navbar-nav" />
-        <Navbar.Collapse id="basic-navbar-nav">
-          <Nav className="ms-auto">
-            <Nav.Link as={NavLink} to="/" className="nav-link">
-              Início
+
+        <Nav className="justify-content-sm-center">
+          <Nav.Link as={NavLink} to="/" className="nav-link">
+            <FiHome className="icon" />
+          </Nav.Link>
+          <Nav.Link as={NavLink} to="/" className="nav-link">
+            <FiImage className="icon" />
+          </Nav.Link>
+          {isLoggedIn ? (
+            <Nav.Link as={NavLink} to={getProfileUrl()} className="nav-link">
+              <FiUser className="icon" />
             </Nav.Link>
-            {!isLoggedIn && (
-              <Nav.Link as={NavLink} to="/cadastro" className="nav-link">
-                Cadastro
-              </Nav.Link>
-            )}
-            {isLoggedIn ? (
-              <Button variant="outline-primary" onClick={handleLogout}>
-                Logout
-              </Button>
-            ) : (
-              <Button as={NavLink} to="/login" variant="outline-primary">
-                Login
-              </Button>
-            )}
-          </Nav>
-        </Navbar.Collapse>
+          ) : (
+            <Nav.Link as={NavLink} to="/cadastro" className="nav-link">
+              <FiUser className="icon" />
+            </Nav.Link>
+          )}
+        </Nav>
+        <div className="ml-auto">
+          {isLoggedIn ? (
+            <Button variant="outline-primary" onClick={handleLogout}>
+              <FiLogOut /> Logout
+            </Button>
+          ) : (
+            <Button as={NavLink} to="/login" variant="outline-primary">
+              <FiLogIn /> Login
+            </Button>
+          )}
+        </div>
       </Container>
     </Navbar>
   );
